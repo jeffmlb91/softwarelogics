5bf8087e591643fccca97243ad4c990694f38a34
jeffmlb91 <jeffmlb91@gmail.com>
2022-09-11 12:01:33 -0400

    folder structur & global style

---
+++ src/globalStyles.js
@@ -0,0 +1,33 @@
+import styled, {createGlobalStyles} from 'styled-components';
+
+const GlobalStyles = createGlobalStyles`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Montserrat' sans-erif;
+}
+`
+export const Container = styled.div`
+    width: 100%;max-width:1300px;
+    margin: 0 auto;
+    padding: 0 50px;
+
+    @media screen and (max-width: 960px) {
+        padding: 0  30px;
+    }
+`
+export const section = styled.div`
+    color: #fff;
+    padding: 160px 0;
+`
+export const Heading = styled.h2`
+    margin-bottom: 1.4rem;
+    font-size: 3 rem;
+    text-align: center;
+    line-height: 1.1;
+    font-weight: 600;
+    color: ${({lighText}) => (lighText ? '#f7f8fa' : '1c2237')};
+`
+
+export default GlobalStyles
